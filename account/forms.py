cb47ac28d3e0a1e5f553fd15dc3cd444ade471a4
Luan Vu <luanvuhlu@gmail.com>
2016-10-25 20:19:57 +0700

    ADD REGISTER PAGE

--- account/forms.py
+++ account/forms.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 
 class LoginForm(AuthenticationForm):
 	username = forms.CharField(label=u'Tên đăng nhập', max_length=50,
@@ -10,3 +11,9 @@ class LoginForm(AuthenticationForm):
 	password = forms.CharField(label=u'Mật khẩu', max_length=50,
 		widget=forms.TextInput(attrs={'class': 'form-control', name='password'}))
 
+class SignUpForm(UserCreationForm):
+	first_name = forms.CharField(required=True)
+	last_name = forms.CharField(required=True)
+	class Meta:
+		model = Account
+		fields = ('email', 'password1', 'password2', 'date_of_birth', 'address_1', 'address_2', 'address_3', 'city')
--- account/views.py
+++ account/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.contrib import auth                 
+from django.core.context_processors import csrf 
+from django.http import HttpResponseRedirect
+from forms import SignUpForm
 # Create your views here.
+def register_user(request):
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/')
+	context = {}
+	context.update(csrf(request))
+	context['form'] = SignUpForm()
+	reteurn render(request, 'register.html', context)
